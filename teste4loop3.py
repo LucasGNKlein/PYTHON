0f7e3962fc0fe7a7d29adfe089ac8e7300531981
Lucas (Sekth) Klein <86809331+LucasGNKlein@users.noreply.github.com>
2023-09-03 01:49:10 -0300

    Update teste4loop3.py

--- teste4loop3.py
+++ teste4loop3.py
@@ -1,11 +1,4 @@
-# Um jovem mágico escolheu um número secreto.
-# Ele o ocultou em uma variável chamada secret_number.
-# Ele quer que todos que executam seu programa joguem o jogo
-# Adivinhe o número secreto e adivinhem qual número ele escolheu para eles.
-# Quem não adivinhar o número ficará para sempre em um loop infinito! 
-# Infelizmente, ele não sabe como completar o código.
-# Crie um código para ajudar o Mago.
-
+# contagem regressiva de 5 até zero
 counter = 5
 while counter != 0:
     print("Dentro do laço.", counter)
