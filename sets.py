559c6f0a46353f980b02ef0e237a083a94c3f51d
Lucas (Sekth) Klein <86809331+LucasGNKlein@users.noreply.github.com>
2025-05-26 13:36:02 -0300

    Create sets.py
    estudo sobre sets em python

---
+++ sets.py
@@ -0,0 +1,8 @@
+m_fruits = {"morango", "goiaba", "uva", "melancia"}
+b_fruits = {"ameixa", "pera", "banana"}
+
+union_set = m_fruits.union(b_fruits)
+intersection_set = m_fruits.intersection(b_fruits)
+
+print(union_set)
+print(intersection_set)
