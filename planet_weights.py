01e80ede75f7f349d8d1a4be277c10acdac3f87d
Lucas (Sekth) Klein <86809331+LucasGNKlein@users.noreply.github.com>
2025-06-25 09:11:08 -0300

    Create planet_weights.py
    exercicio de loos em python, usuário deve digitar seu peso e um numero para medir o peso de certo pais

---
+++ planet_weights.py
@@ -0,0 +1,26 @@
+earth_weight = float(input('Enter your weight: '))
+planet = int(input('Enter the planet number: '))
+
+if planet == 1:
+    destination_weight = earth_weight * 0.38
+    print(desination_weight)
+elif planet == 2:
+    destination_weight = earth_weight * 0.91
+    print(desination_weight)
+elif planet == 3:
+    destination_weight = earth_weight * 0.38
+    print(desination_weight)
+elif planet == 4:
+    destination_weight = earth_weight * 2.53
+    print(desination_weight)
+elif planet == 5:
+    destination_weight = earth_weight * 1.07
+    print(desination_weight)
+elif planet == 6:
+    destination_weight = earth_weight * 0.89
+    print(desination_weight)
+elif planet == 7:
+    destination_weight = earth_weight * 1.14
+    print(desination_weight)
+else:
+    print('Invalid planet number')
