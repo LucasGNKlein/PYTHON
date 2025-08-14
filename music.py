a142f1f6d6052f509e0f86f232455f1a52a1aafb
Lucas (Sekth) Klein <86809331+LucasGNKlein@users.noreply.github.com>
2025-08-14 20:27:36 -0300

    Add files via upload
    arquivo que faz a descrição da letra de uma música, que pode ser alterada

---
+++ music.py
@@ -0,0 +1,43 @@
+"""Exibe letras de uma música com efeito de digitação e cores usando Rich."""
+
+import sys
+#import time
+from time import sleep
+from rich import print as rich_print
+
+
+def print_lyrics():
+    """Imprime a letra da musica"""
+
+    lines = [
+        ("I wanna da-", 0.06),
+        ("I wanna dance in the lights", 0.05),
+        ("I wanna ro-", 0.07),
+        ("I wanna rock your body", 0.08),
+        ("I wanna go", 0.08),
+        ("I wanna go for a ride", 0.068),
+        ("Hop in the music and", 0.07),
+        ("Rock your body", 0.08),
+        ("Rock that body",0.069),
+        ("come on, come on", 0.035),
+        ("Rock that body", 0.05),
+        ("(Rock your body)", 0.03),
+        ("Rock that body", 0.049),
+        ("come on, come on", 0.035),
+        ("Rock that body", 0.08),
+    ]
+
+    delays = [0.2, 1, 0.2, 1, 0.2, 0.8, 0.2, 0.5, 0.18, 0.1, 0.15, 0.3, 0.3, 0.1, 5]
+
+    for i, (line, char_delay) in enumerate(lines):
+        for char in line:
+            if line == '(Rock your body)':
+                rich_print(f"[orange4]{char}[/orange4]", end="")
+            else:
+                rich_print(f"[bold][gold3]{char}[/bold][/gold3]", end="")
+            sys.stdout.flush()
+            sleep(char_delay)
+        print()
+        sleep(delays[i])
+
+print_lyrics()
